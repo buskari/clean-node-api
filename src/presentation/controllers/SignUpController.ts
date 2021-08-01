e9edb911e12ed7d374be68196a4300ae3578eb5f
Andris Buscariolli <andris.contact@gmail.com>
2021-08-01 19:45:25 -0300

    feat: ensure that SignUpController returns 400 if NO NAME is provided

---
+++ src/presentation/controllers/SignUpController.ts
@@ -0,0 +1,10 @@
+export class SignUpController {
+  handle (httpRequest: any): any {
+    // const data = httpRequest.body
+
+    // if (!httpRequest.body.name)
+    return {
+      statusCode: 400
+    }
+  }
+}
