3b7bcb6946f0fe5121fe7f48a020f0c5da0f74f8
Andris Buscariolli <andris.contact@gmail.com>
2021-08-05 08:31:45 -0300

    test: ensure SingUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -217,4 +217,24 @@ describe('SignUp Controller ', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
   })
+
+  it('Should return 200 if valid data is provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'test_name',
+        email: 'test_email@mail.com',
+        password: 'test_password',
+        passwordConfirmation: 'test_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'valid_password'
+    })
+  })
 })
