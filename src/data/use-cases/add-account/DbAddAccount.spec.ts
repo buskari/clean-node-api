b1ce4a7bb775680cb125e43866c465e93e595b6c
Andris Buscariolli <andris.contact@gmail.com>
2021-08-13 13:07:59 -0300

    refactor: group all interface in a single file

--- src/data/use-cases/add-account/DbAddAccount.spec.ts
+++ src/data/use-cases/add-account/DbAddAccount.spec.ts
@@ -1,4 +1,4 @@
-import { Encrypter } from './../../interfaces/EncrypterInterface'
+import { Encrypter } from './DbAddAccountInterfaces'
 import { DbAddAccount } from './DbAddAccount'
 
 interface SutInterface {
---
+++ src/data/use-cases/add-account/DbAddAccountInterfaces.ts
@@ -0,0 +1,4 @@
+/* istanbul ignore file */
+export * from './../../interfaces/EncrypterInterface'
+export * from './../../../domain/models/AccountModel'
+export * from './../../../domain/use-cases/AddAccount'
